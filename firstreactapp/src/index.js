d617602245a2a0d84785c8126c8cccd922e514b6
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-04-02 11:54:35 +0500

    App component

--- firstreactapp/src/index.js
+++ firstreactapp/src/index.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MyBtn from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-  <MyBtn text='Click me' heading='First Heading'/>
-  <MyBtn text='Second button' heading='Second Heading' />
+    <App />
   </>
-  
 );
-
